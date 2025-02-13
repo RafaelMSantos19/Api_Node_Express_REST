bd7086e428275f31b65dfcf29ec44d4a6c2084ae
Rafael Santos <rafarag40@gmail.com>
2025-02-13 16:09:24 -0300

    feat: routers is functional

--- index.js
+++ index.js
@@ -4,32 +4,89 @@ const pool = require('./connections/db')
 const express = require('express')
 const app = express()
 
-// Middleware para parsear JSON
 app.use(express.json());
 
-// Rota GET simples
-app.get('/getAll', (req, res) => {
+
+
+function appendWhere(headers){
+
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+    console.log(headers)
+    
+    whereArray= [] 
+
+    const { id, valor, status, metodo_pagamento} = headers
+
+    if(id !=null){
+        whereArray.push(`id = ${id}`)
+    }
+    if(valor != null && valor != '' && valor != undefined){
+        whereArray.push(`valor = ${valor}`)
+    }
+    if(status != null){
+        whereArray.push(`status = '${status}'`)
+    }
+    if(metodo_pagamento != null && metodo_pagamento != '' && metodo_pagamento != undefined){
+        whereArray.push(`metodo_pagamento = '${metodo_pagamento}'`)
+    }
+            
+
+    if(whereArray.length == 0){
+
+        return null
+    }else{
+
+        where = "where "+whereArray.join(" AND ")
+
+
+        return where
+    }
+    
+}
 
 
 
+app.get('/getTransactions', async (req, res) => {
 
-    //res.send('Rota para receber tudo disponivel no banco de dados');
+    try{
+
+        where = appendWhere(req.headers)
+
+        console.log(where)
+
+
+
+        if(where != null){
+
+            StringQuery = `select * from transactions ${where}`
+
+        }else{     
+  
+            StringQuery = "select * from transactions"
+        }
+
+
+        const transactions = await pool.query(StringQuery)
+        res.send(transactions.rows);
+
+    }catch(erro){
+        console.error("Um erro ocorreu:")
+        console.error(erro.mensagem)
+        res.status(500).send('Erro a o Exibir Transação');
+    }
+
 });
 
 
-// Rota GET simples
-app.get('/gettransactions', (req, res) => {
-    res.send('Rota para pega transações com uso de filtros');
-  });
-
-// Rota POST exemplo
 app.post('/newTransaction', async (req, res) => {
 
     try{
+
         const { valor, status, metodo_pagamento} = req.body
 
         const newTransaction = await pool.query(
-            `INSERT INTO transactions (valor, status, metodo_pagamento) VALUES (${valor}, ${status}, ${metodo_pagamento})`
+            `INSERT INTO transactions (valor, status, metodo_pagamento) VALUES (${valor}, '${status}', '${metodo_pagamento}')`
         )
 
         res.json(newTransaction.rows[0])
